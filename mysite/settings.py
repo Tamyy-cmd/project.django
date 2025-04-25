81e3aa1431421cacfcf4ec51c66c84d1a036f1a4
Tamyy-cmd <tamillyduartedossantos@gmail.com>
2025-04-25 18:19:40 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hp*z-z$7_*5zmb^xr0&@qwo$w%_h=^9=rmu@4k$#(^nwm8hrj5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['15.229.8.27', 'localhost', '127.0.0.1']
 
 # Application definition
 
